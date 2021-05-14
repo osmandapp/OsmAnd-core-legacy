6512343618dafed760102df1aba6e3cd0385f9e6
PaulStets <stetsenko.pavlo@gmail.com>
2021-05-14 18:04:20 +0300

    Custom routing xml support ios

--- native/src/routingConfiguration.h
+++ native/src/routingConfiguration.h
@@ -58,10 +58,9 @@ struct RoutingConfiguration {
 
 class RoutingConfigurationBuilder {
 private:
-    MAP_STR_STR attributes;
     UNORDERED(map)<int64_t, int_pair> impassableRoadLocations;
-
 public:
+    MAP_STR_STR attributes;
     UNORDERED(map)<string, SHARED_PTR<GeneralRouter> > routers;
     string defaultRouter;
 
