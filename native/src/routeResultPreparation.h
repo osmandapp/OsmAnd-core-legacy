0d98f4df35c3a8d78393e60cd3fd64cddfd07a93
alisa911 <alisa.91150@gmail.com>
2021-10-26 10:05:28 +0300

    fix turns

--- native/src/routeResultPreparation.h
+++ native/src/routeResultPreparation.h
@@ -1,3 +1,5 @@
+//  git revision ab36d0ce1a089ab1cf8dd8c582ac9e85cd77bcff
+
 #ifndef _OSMAND_ROUTE_RESULT_PREPARATION_H
 #define _OSMAND_ROUTE_RESULT_PREPARATION_H
 #include "CommonCollections.h"
@@ -21,5 +23,6 @@ vector<SHARED_PTR<RouteSegmentResult> > prepareResult(RoutingContext* ctx, vecto
 vector<int> parseTurnLanes(const SHARED_PTR<RouteDataObject>& ro, double dirToNorthEastPi);
 vector<int> parseLanes(const SHARED_PTR<RouteDataObject>& ro, double dirToNorthEastPi);
 void prepareTurnResults(RoutingContext* ctx, vector<SHARED_PTR<RouteSegmentResult> >& result);
+SHARED_PTR<TurnType> getTurnByCurrentTurns(std::vector<std::vector<int>> lanesInfo, vector<int>& rawLanes, int keepTurnType, bool leftSide);
 
 #endif /*_OSMAND_ROUTE_RESULT_PREPARATION_H*/
