f9682efca38007aec598cf2ce464cf3510d1876a
RZR-UA <youonly@ukr.net>
2023-11-01 14:09:52 +0100

    Speed up haversine C++ getDistance() by ~10%

--- native/src/common.cpp
+++ native/src/common.cpp
@@ -282,8 +282,9 @@ double getDistance(double lat1, double lon1, double lat2, double lon2) {
 	double R = 6372.8;  // km
 	double dLat = toRadians(lat2 - lat1);
 	double dLon = toRadians(lon2 - lon1);
-	double a =
-		sin(dLat / 2) * sin(dLat / 2) + cos(toRadians(lat1)) * cos(toRadians(lat2)) * sin(dLon / 2) * sin(dLon / 2);
+	double sinHalfLat = sin(dLat / 2);
+	double sinHalfLon = sin(dLon / 2);
+	double a = sinHalfLat * sinHalfLat + cos(toRadians(lat1)) * cos(toRadians(lat2)) * sinHalfLon * sinHalfLon;
 	double c = 2 * atan2(sqrt(a), sqrt(1 - a));
 	return R * c * 1000;
 }
