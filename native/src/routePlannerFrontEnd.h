73a44f4475f5e95e4f387005ee95c8cd649d550f
RZR-UA <youonly@ukr.net>
2024-02-14 08:27:19 +0100

    Fix formatting

--- native/src/routePlannerFrontEnd.h
+++ native/src/routePlannerFrontEnd.h
@@ -13,7 +13,7 @@ struct RouteSegment;
 struct RoutingConfiguration;
 
 struct GpxRouteApproximation {
-	
+
 	RoutingContext* ctx;
 	int routeCalculations = 0;
 	int routePointsSearched = 0;
@@ -87,7 +87,7 @@ public:
 	
 	void searchGpxRoute(SHARED_PTR<GpxRouteApproximation>& gctx, vector<SHARED_PTR<GpxPoint>>& gpxPoints, GpxRouteApproximationCallback acceptor = nullptr);
 	static bool hasSegment(vector<SHARED_PTR<RouteSegmentResult>>& result, SHARED_PTR<RouteSegment>& current);
-    HHRoutingConfig * setDefaultRoutingConfig();
+	HHRoutingConfig* setDefaultRoutingConfig();
 	
 private:
 	
@@ -110,7 +110,7 @@ private:
 	bool pointCloseEnough(SHARED_PTR<GpxRouteApproximation>& gctx, SHARED_PTR<GpxPoint>& ipoint,
 	                      vector<SHARED_PTR<RouteSegmentResult>>& res);
 	void makeSegmentPointPrecise(SHARED_PTR<RouteSegmentResult>& routeSegmentResult, double lat, double lon, bool st);
-    HHNetworkRouteRes * calculateHHRoute(HHRoutePlanner & routePlanner, RoutingContext* ctx, int startX, int startY, int endX, int endY, double dir);
+	HHNetworkRouteRes* calculateHHRoute(HHRoutePlanner & routePlanner, RoutingContext* ctx, int startX, int startY, int endX, int endY, double dir);
 };
 
 #endif /*_OSMAND_ROUTE_PLANNER_FRONT_END_H*/
