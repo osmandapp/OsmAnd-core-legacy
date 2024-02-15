30384f86e7ca31270a71929a88a9dffac3fb689e
RZR-UA <90444451+RZR-UA@users.noreply.github.com>
2024-02-15 14:29:23 +0100

    [Core-legacy] warns, syntax, code improvements (#176)
    * Add -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unused-function
    
    * Clarify -std=c++0x to -std=c++11, fix few warinngs

--- targets/.cmake/targets/amd64-linux-clang.cmake
+++ targets/.cmake/targets/amd64-linux-clang.cmake
@@ -3,6 +3,5 @@ set(CMAKE_TARGET_CPU_ARCH amd64)
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_COMPILER_FAMILY clang)
-set(CMAKE_C_COMPILER_FLAGS "-m64 -msse4.1 -fstack-protector-all -Wstack-protector")
-set(CMAKE_CXX_COMPILER_FLAGS "-std=c++0x -m64 -msse4.1 -fstack-protector-all -Wstack-protector")
- 
+set(CMAKE_C_COMPILER_FLAGS "-m64 -msse4.1 -fstack-protector-all -Wstack-protector -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unused-function")
+set(CMAKE_CXX_COMPILER_FLAGS "-std=c++11 -m64 -msse4.1 -fstack-protector-all -Wstack-protector -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unused-function")
