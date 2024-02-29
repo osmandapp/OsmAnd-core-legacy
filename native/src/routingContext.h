3509ace76d6191c81bc96e0fd6bb7e72e3c790c2
RZR-UA <youonly@ukr.net>
2024-02-29 19:44:09 +0100

    Add ctx->isCalledFromJava (true by java wrapper)

--- native/src/routingContext.h
+++ native/src/routingContext.h
@@ -76,6 +76,8 @@ inline int compareRoutingSubregionTile(SHARED_PTR<RoutingSubregionTile>& o1, SHA
 }
 
 struct RoutingContext {
+	bool isCalledFromJava; // default is false, changed by getRoutingContext() in java_wrap.cpp
+
 	typedef UNORDERED(map)<int64_t, SHARED_PTR<RoutingSubregionTile>> MAP_SUBREGION_TILES;
 
 	RouteCalculationMode calculationMode;
@@ -135,11 +137,13 @@ struct RoutingContext {
 		this->calculationProgressFirstPhase = std::make_shared<RouteCalculationProgress>();
 		this->alertFasterRoadToVisitedSegments = 0;
 		this->alertSlowerSegmentedWasVisitedEarlier = 0;
+		this->isCalledFromJava = cp->isCalledFromJava;
 	}
 
 	RoutingContext(SHARED_PTR<RoutingConfiguration> config,
 				   RouteCalculationMode calcMode = RouteCalculationMode::NORMAL)
-		: calculationMode(calcMode), config(config), progress(new RouteCalculationProgress()),
+		: isCalledFromJava(false),
+		  calculationMode(calcMode), config(config), progress(new RouteCalculationProgress()),
 		  calculationProgressFirstPhase(new RouteCalculationProgress()), leftSideNavigation(false),
 		  startTransportStop(false), targetTransportStop(false), publicTransport(false), geocoding(false),
 		  conditionalTime(0), precalcRoute(new PrecalculatedRouteDirection()), alertFasterRoadToVisitedSegments(0),
