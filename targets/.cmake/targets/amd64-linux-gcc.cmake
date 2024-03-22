d54a633a37096b87440de954b0092e12f4a3a5a1
RZR-UA <youonly@ukr.net>
2024-03-22 10:08:27 +0100

    Revert "Add -O2 to default cmake scripts for core-legacy"
    This reverts commit 4c5b4a3fc1cc7d44b8f8c00041ccf23b505479d2.

--- targets/.cmake/targets/amd64-linux-gcc.cmake
+++ targets/.cmake/targets/amd64-linux-gcc.cmake
@@ -3,6 +3,6 @@ set(CMAKE_TARGET_CPU_ARCH amd64)
 set(CMAKE_C_COMPILER gcc)
 set(CMAKE_CXX_COMPILER g++)
 set(CMAKE_COMPILER_FAMILY gcc)
-set(CMAKE_C_COMPILER_FLAGS "-O2 -m64 -msse4.1 -fstack-protector-all -Wstack-protector -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unused-function")
-set(CMAKE_CXX_COMPILER_FLAGS "-O2 -std=c++11 -m64 -msse4.1 -fstack-protector-all -Wstack-protector -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unused-function")
+set(CMAKE_C_COMPILER_FLAGS "-m64 -msse4.1 -fstack-protector-all -Wstack-protector -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unused-function")
+set(CMAKE_CXX_COMPILER_FLAGS "-std=c++11 -m64 -msse4.1 -fstack-protector-all -Wstack-protector -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unused-function")
  
