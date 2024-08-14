32d34a4e2723d14db0f1b35fa865d688dc518770
RZR-UA <youonly@ukr.net>
2024-08-14 12:24:19 +0200

    Hotfix int->float in calculateTurnTime()

--- native/src/generalRouter.cpp
+++ native/src/generalRouter.cpp
@@ -576,12 +576,12 @@ double GeneralRouter::calculateTurnTime(const SHARED_PTR<RouteSegment>& segment,
 		double a1 = segment->getRoad()->directionRoute(segment->getSegmentStart(), segment->isPositive());
 		double a2 = prev->getRoad()->directionRoute(prev->getSegmentEnd(), !prev->isPositive());
 		double diff = abs(alignAngleDifference(a1 - a2 - M_PI));
-		if (diff > M_PI / 1.5) {
+		if (diff > M_PI / 1.5f) {
 			totalPenalty += sharpTurn; // >120 degree (like U-turn)
-		} else if (diff > M_PI / 3) {
+		} else if (diff > M_PI / 3.0f) {
 			totalPenalty += slightTurn; // >60 degree (fixed penalty)
-		} else if (diff > M_PI / 6) {
-			totalPenalty += slightTurn * diff * 3 / M_PI; // >30 (linear penalty)
+		} else if (diff > M_PI / 6.0f) {
+			totalPenalty += slightTurn * diff * 3.0f / M_PI; // >30 (linear penalty)
 		}
 	}
 	return totalPenalty;
